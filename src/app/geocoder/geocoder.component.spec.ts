7bc8e9621c7172cf273bddd4e9b5e32c13978330
Huascar <huascarmc@hotmail.com>
2018-02-07 20:47:17 +0000

    can't access properties of geocoder after making request on test

--- src/app/geocoder/geocoder.component.spec.ts
+++ src/app/geocoder/geocoder.component.spec.ts
@@ -25,43 +25,57 @@ describe('GeocoderComponent', () => {
   //   expect(component).toBeTruthy();
   // });
 
-  it(`should have has 'address' property`, async(() => {
+  it(`should have 'address' property`, async(() => {
     // const fixture = TestBed.createComponent(GeocoderComponent);
     const GeocoderComponent = fixture.debugElement.componentInstance;
     expect(GeocoderComponent.address).toEqual(undefined);
   }));
 
 
-  it(`should have has 'lat' property`, async(() => {
+  it(`should have 'lat' property`, async(() => {
     // const fixture = TestBed.createComponent(GeocoderComponent);
     const GeocoderComponent = fixture.debugElement.componentInstance;
     expect(GeocoderComponent.lat).toEqual(undefined);
   }));
 
 
-  it(`should have has 'lng' property`, async(() => {
+  it(`should have 'lng' property`, async(() => {
     // const fixture = TestBed.createComponent(GeocoderComponent);
     const GeocoderComponent = fixture.debugElement.componentInstance;
     expect(GeocoderComponent.lng).toEqual(undefined);
   }));
 
-  it(`should have has 'api_key' property`, async(() => {
+  it(`should have 'api_key' property`, async(() => {
     // const fixture = TestBed.createComponent(GeocoderComponent);
     const GeocoderComponent = fixture.debugElement.componentInstance;
     expect(GeocoderComponent.api_key).toEqual('AIzaSyDaWjtIBo52j7qrtJKsr7ykERqQ5MywinE');
   }));
 
-  it(`should have has 'error' property`, async(() => {
+  it(`should have 'error' property`, async(() => {
     // const fixture = TestBed.createComponent(GeocoderComponent);
     const GeocoderComponent = fixture.debugElement.componentInstance;
     expect(GeocoderComponent.error).toEqual('');
   }));
 
-  it(`should have has 'searchCount' property`, async(() => {
+  it(`should have 'searchCount' property`, async(() => {
     // const fixture = TestBed.createComponent(GeocoderComponent);
     const GeocoderComponent = fixture.debugElement.componentInstance;
     expect(GeocoderComponent.searchCount).toEqual(1);
   }));
 
+  xit(`should update 'latitude'`, async(() => {
+   const GeocoderComponent = fixture.debugElement.componentInstance;
+   GeocoderComponent.address = '37-39 Cowgate Edinburgh';
+   GeocoderComponent.makeRequest();
+   console.log(GeocoderComponent)
+   expect(GeocoderComponent.lat).toEqual(53);
+  }));
 
+  xit(`should update 'longitude'`, async(() => {
+   const GeocoderComponent = fixture.debugElement.componentInstance;
+   GeocoderComponent.address = '37-39 Cowgate Edinburgh';
+   GeocoderComponent.makeRequest();
+   console.log(GeocoderComponent.lng)
+   expect(GeocoderComponent.lng).toEqual(-3);
+  }));
 });
