56bf670f7f79b628abbd64085fa653f34da4b96a
Huascar <huascarmc@hotmail.com>
2018-02-07 19:20:38 +0000

    request method compile success

--- src/app/geocoder/geocoder.component.ts
+++ src/app/geocoder/geocoder.component.ts
@@ -9,29 +9,31 @@ import { HttpClient } from '@angular/common/http';
 
 export class GeocoderComponent implements OnInit {
 
-  address: string = null;
-  lat: string = null;
-  lng: string = null;
+  address: string;
+  lat: string;
+  lng: string;
   api_key: string = 'AIzaSyDaWjtIBo52j7qrtJKsr7ykERqQ5MywinE';
   error: string = ''
   searchCount = 0;
 
   constructor(private http: HttpClient) { }
 
-  makeRequest(): void {
-   this.http.get(
-   'https://maps.googleapis.com/maps/api/geocode/json?address='
-    + this.address + '&key=' + this.api_key).subscribe(data => {
-      if(data.status != "ZERO_RESULTS") {
-        this.lat = (data.results[0].geometry.location.lat).toString();
-        this.lng = (data.results[0].geometry.location.lng).toString();
-        this.error = ''
-        console.log(data);
-      } else {
-       this.error = 'Not found'
-      }
-    });
+  populateInfo(info): void {
+   if(info.results[0] != undefined) {
+    this.lat = (info.results[0].geometry.location.lat).toString();
+    this.lng = (info.results[0].geometry.location.lng).toString();
+    this.error = ''
+   } else {
+    this.error = 'Not found'
+   }
   }
+
+  makeRequest(): void {
+   this.http.get<object>(`https://maps.googleapis.com/maps/api/geocode/json?address=`
+    + this.address + '&key=' + this.api_key).subscribe(data => {
+      this.populateInfo(data)
+    });
+   }
 
   ngOnInit(): void {
     if (this.searchCount > 0) {
